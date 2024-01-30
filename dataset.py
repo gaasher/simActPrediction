1ead3a1ec91dfa8041e41ef41637d09d688e32ab
gaasher <gabrielasher2002@gmail.com>
2024-01-29 20:09:42 -0500

    added some additional augemntations for random noise and random channel removal/noise

--- dataset.py
+++ dataset.py
@@ -3,14 +3,34 @@ import zarr
 from torch.utils.data import Dataset
 
 class simAPDataset(Dataset):
-    def __init__(self, path, stage, transform=None):
+    def __init__(self, path, stage, do_transform=False, transforms=None):
         super().__init__()
         self.path = path
         self.stage = stage
-        self.transform = transform # for now we will just use the raw data, but if we want to add augmentations
+        self.do_transform = do_transform
+        self.transforms = ['noise', 'channel_off']
 
         self.data = zarr.open(f'{self.path}/{stage}_data.zarr', mode='r')
         self.labels = zarr.open(f'{self.path}/{stage}_labels.zarr', mode='r')
+
+
+    # create augmentation functions for 1) turn random channels into noise 2) add noise to random parts of the signal
+    def channel_off(self, x, perc_off=0.2):
+        #shape of x is (num_channels, 128)
+        num_off = int(perc_off * x.shape[0])
+        off_idx = np.random.choice(x.shape[0], num_off, replace=False)
+        x[off_idx] = np.random.normal(0, 1, (num_off, 128))
+        return x
+    
+    def add_noise(self, x, perc_noise=0.2):
+        #shape of x is (num_channels, 128)
+        noise_idx = np.random.choice(x.shape[1], int(perc_noise * x.shape[1]), replace=False)
+        x[:, noise_idx] = np.random.normal(0, 1, (x.shape[0], len(noise_idx)))
+        return x
+    
+    # add normalizer, unit norm for now
+    def normalize(self, x):
+        return x / np.linalg.norm(x, axis=1, keepdims=True)
 
     def __len__(self):
         return len(self.data)
@@ -19,8 +39,14 @@ class simAPDataset(Dataset):
         x = self.data[idx]
         y = self.labels[idx]
 
+        x = self.normalize(x)
 
+        # add augmentation here
+        if self.do_transform:
+            if 'noise' in self.transforms:
+                x = self.add_noise(x)
+            if 'channel_off' in self.transforms:
+                x = self.channel_off(x)
         # add normalization here
-        if self.transform:
-            x = self.transform(x)
+        
         return x, y
